fc377678f1a5fd50e979bd79e5f117eaa979b387
idris-zinai <idriszinai92@gmail.com>
2025-06-21 21:45:54 +0100

    Delete devoir-5-vowel-or-consonant directory

--- devoir-5-vowel-or-consonant/main.py
+++
@@ -1,5 +0,0 @@
-lettre = input("Entrez une lettre : ")
-if  lettre==("a" or "e" or "i" or "o" or "u") :
-       print("vous avez entrer une voyelle")
-else :
-       print("vous avez entrer une consonne")
